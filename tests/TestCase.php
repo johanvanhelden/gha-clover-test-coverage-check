0f4e17c9e64b52c4e1bcd361c60b5385c144a812
emibcn <emi@algorismia.com>
2020-09-25 18:07:39 +0200

    Fix indentation?

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,12 +9,11 @@ use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 abstract class TestCase extends PHPUnitTestCase
 {
     protected function runCoverageCheck(
-	string $file,
-	string $percentage = '50',
-	string $roundedPrecision = '2',
-	string $metric = 'elements'
-    ): string
-    {
+        string $file,
+        string $percentage = '50',
+        string $roundedPrecision = '2',
+        string $metric = 'elements'
+    ): string {
         $argv = [];
         $argv[1] = __DIR__ . '/Fixtures/' . $file;
         $argv[2] = $percentage;
